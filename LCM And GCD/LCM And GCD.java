54aa0a6746fc2af7a11eaddfbf211468abb14189
Safwan Nasir <127377148+safwannasir49@users.noreply.github.com>
2025-02-08 11:13:07 +0530

    Create LCM And GCD.java

---
+++ LCM And GCD/LCM And GCD.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int a;
+            a = Integer.parseInt(br.readLine());
+
+            int b;
+            b = Integer.parseInt(br.readLine());
+
+            Solution obj = new Solution();
+            int[] ans = obj.lcmAndGcd(a, b);
+            System.out.println(ans[0] + " " + ans[1]);
+            System.out.println("~");
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+// User function Template for Java
+
+class Solution {
+    public static int[] lcmAndGcd(int a, int b) {
+        int gcd = findGCD(a, b);
+        int lcm = (a / gcd) * b; 
+        return new int[]{lcm, gcd};
+    }
+
+    private static int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
